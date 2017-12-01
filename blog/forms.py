d13f66b1d839a1850293e1e22ab52399c7f514eb
Emmanuel de Revel <emmanuel.de-revel@student.ecp.fr>
2017-12-01 10:52:08 +0100

    css

--- blog/forms.py
+++ blog/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class ConnexionForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput())
-    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    username = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class': 'w3-input w3-border', 'placeholder': 'username'}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'w3-input w3-border', 'placeholder': 'password'}))
